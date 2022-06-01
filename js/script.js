c69e9a07969b9acc92b948a398eb1fd1b45f23f6
Gius993 <giuseppevidili9@gmail.com>
2022-06-01 15:46:58 +0200

    costruzione programma quasi ultimata

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Anni
+var age = parseInt(prompt("Dimmi la tua età"));
+
+console.log(age);
+//Distanza
+var km = parseInt(prompt("Dimmi quanti km devi fare"));
+
+console.log(km);
+//Prezzi
+var ticketPrice = km * 0.21;
+
+console.log(ticketPrice);
+
+var discount20 = ticketPrice * 0.2;
+
+var discount40 = ticketPrice * 0.4;
+//Calcoli
+
+if (age < 18) {
+var ticketPrice = ticketPrice - discount20;
+} else if (age > 65) {
+var ticketPrice = ticketPrice - discount40;
+} else {
+var ticketPrice = ticketPrice;
+}
+//fix decimali
+console.log(ticketPrice.toFixed(2));
+console.log(ticketPrice - discount20.toFixed(2));
+console.log(ticketPrice - discount40.toFixed(2));
+
+
+//Stampa
+document.getElementById("ticket").innerHTML = "Il prezzo del tuo biglietto è: " + ticketPrice + " euro";
